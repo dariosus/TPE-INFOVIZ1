a939f432c493143147a2ae7db893a8dd13a616a6
Dario Susnisky <dario15th@gmail.com>
2012-05-17 13:16:22 -0300

    Visualizacion 1

--- TPE1-INFOVIZ/src/models/Comparators.java
+++ TPE1-INFOVIZ/src/models/Comparators.java
@@ -7,7 +7,7 @@ public class Comparators {
 	public Comparator<Function> getLinesComparator() {
 		return new Comparator<Function>() {
 			public int compare(Function a, Function b) {
-				return a.getLines() < b.getLines() ? -1 : a.getLines() == b.getLines() ? 0 : 1;
+				return a.getLines() < b.getLines() ? -1 : 1;
 			}
 		};
 	}
@@ -15,7 +15,7 @@ public class Comparators {
 	public Comparator<Function> getParametersComparator() {
 		return new Comparator<Function>() {
 			public int compare(Function a, Function b) {
-				return a.getParameters() < b.getParameters() ? -1 : a.getParameters() == b.getParameters() ? 0 : 1;
+				return a.getParameters() < b.getParameters() ? -1 : 1;
 			}
 		};
 	}
